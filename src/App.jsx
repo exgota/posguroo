4129ff347cfc8c22fb5dd23f4ec1693ceb2fa8c5
exgota <87779273+exgota@users.noreply.github.com>
2025-06-12 12:49:52 -0400

    Add React Vite project with SpotHopper iframe

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <div style={{height: '100vh'}}>
+      <iframe
+        src="https://www.spothopperapp.com/"
+        title="SpotHopper"
+        style={{width: '100%', height: '100%', border: 'none'}}
+      />
+    </div>
+  );
+}
+
+export default App;
--- utils/useCopy.ts
+++
@@ -1,5 +0,0 @@
-import copy from '../content/copy.json';
-import kpis from '../content/kpis.json';
-
-export const useCopy = () => copy;
-export const useKpis = () => kpis;
